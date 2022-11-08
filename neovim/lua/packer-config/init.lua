4a1e8ea24a89e69ce1291142fa6b93f512bf79cc
Damet24 <damet.2401@gmail.com>
2022-11-07 20:22:51 -0500

    polybar updated and plugin for neovim

--- neovim/lua/packer-config/init.lua
+++ neovim/lua/packer-config/init.lua
@@ -1,47 +1,52 @@
 return require 'packer'.startup(function()
-  use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
 
-  -- theme
-  use 'morhetz/gruvbox'
-  use 'https://github.com/bluz71/vim-moonfly-colors'
-  use 'navarasu/onedark.nvim'
-  use 'folke/tokyonight.nvim'
-  use 'patstockwell/vim-monokai-tasty'
+    -- theme
+    use 'morhetz/gruvbox'
+    use 'https://github.com/bluz71/vim-moonfly-colors'
+    use 'navarasu/onedark.nvim'
+    use 'folke/tokyonight.nvim'
+    use 'patstockwell/vim-monokai-tasty'
 
-  use 'glepnir/dashboard-nvim'
+    use 'glepnir/dashboard-nvim'
 
-  -- utils
-  use { 'kyazdani42/nvim-web-devicons' }
-  use { 'kyazdani42/nvim-tree.lua' }
-  use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
+    -- utils
+    use { 'kyazdani42/nvim-web-devicons' }
+    use { 'kyazdani42/nvim-tree.lua' }
+    use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
 
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
-  use "lukas-reineke/indent-blankline.nvim"
+    use "lukas-reineke/indent-blankline.nvim"
 
-  use { 'ibhagwan/fzf-lua', requires = { 'kyazdani42/nvim-web-devicons' } }
+    use { 'ibhagwan/fzf-lua', requires = { 'kyazdani42/nvim-web-devicons' } }
 
-  use "terrortylor/nvim-comment"
+    use "terrortylor/nvim-comment"
 
-  use 'voldikss/vim-floaterm'
+    use 'voldikss/vim-floaterm'
 
-  use {
-    "windwp/nvim-autopairs",
-    config = function() require("nvim-autopairs").setup {} end
-  }
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
 
-  use 'williamboman/nvim-lsp-installer'
-  use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/nvim-cmp'
-  use 'L3MON4D3/LuaSnip'
-  use 'saadparwaiz1/cmp_luasnip'
+    use {
+        'romgrk/barbar.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons' }
+    }
 
-  use 'windwp/nvim-ts-autotag'
+    use 'williamboman/nvim-lsp-installer'
+    use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+    use 'hrsh7th/nvim-cmp'
+    use 'L3MON4D3/LuaSnip'
+    use 'saadparwaiz1/cmp_luasnip'
 
-  use 'nvim-lua/plenary.nvim'
-  use 'mhinz/vim-startify'
+    use 'windwp/nvim-ts-autotag'
+
+    use 'nvim-lua/plenary.nvim'
+    use 'mhinz/vim-startify'
 end)
