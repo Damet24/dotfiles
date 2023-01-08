23bdba8b7ec61fc3d8b6131988e53dd791d4a99f
Damet24 <damet.2401@gmail.com>
2023-01-08 14:10:52 -0500

    [neovim] keybindings updated

--- neovim/lua/core/mappings.lua
+++ neovim/lua/core/mappings.lua
@@ -12,9 +12,11 @@ map('n', '<A-t>', ':ToggleTerm direction=horizontal<CR>', opts)
 map('t', '<A-t>', '<Esc> <C-\\><C-n>:ToggleTerm<CR>', opts)
 
 -- Save and close
-map('n', '<C-s>', ':w<CR>', opts)
+-- map('n', '<C-s>', ':w<CR>', opts)
 
 -- telescope
 map('n', '<leader>ff',
-  ":lua require('telescope.builtin').find_files({layout_strategy='vertical',layout_config={width=0.5}})<CR>"
-  , opts)
+	":lua require('telescope.builtin').find_files({layout_strategy='vertical',layout_config={width=0.5}})<CR>"
+	, opts)
+
+
