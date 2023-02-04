c1118bd8c240dd937f98e55e4819c520181a9aa1
Damet24 <damet.2401@gmail.com>
2023-02-04 01:36:59 -0500

    [scripts] change layout script updated

--- scripts/layout.sh
+++ scripts/layout.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+current_layout=$(setxkbmap -query | grep layout | awk '{print $2}')
+
+if test $current_layout == 'dvorak'; then
+  setxkbmap es
+  notify-send "Layout was changed"  "Español" -t 2
+else
+  setxkbmap dvorak
+  notify-send "Layout was changed"  "Dvorak" -t 2
+fi
