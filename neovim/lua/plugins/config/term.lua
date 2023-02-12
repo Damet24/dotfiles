655533d4bbb30b75688e579a9303ac585b10576b
Damet24 <damet.2401@gmail.com>
2023-02-11 20:26:01 -0500

    [neovim] tmux pluging change

--- neovim/lua/plugins/config/term.lua
+++ neovim/lua/plugins/config/term.lua
@@ -1,7 +1,7 @@
 require("toggleterm").setup()
 
-local Terminal     = require('toggleterm.terminal').Terminal
-local lazygit      = Terminal:new({ cmd = "lazygit", hidden = true, size = 20 })
+local Terminal = require('toggleterm.terminal').Terminal
+local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true, size = 20 })
 
 function _lazygit_toggle()
   lazygit:toggle()
--- neovim/lua/plugins/init.lua
+++ neovim/lua/plugins/init.lua
@@ -3,10 +3,10 @@ require('packer').startup(function(use)
 
   -- nvim tree
   use {
-    'nvim-tree/nvim-tree.lua',
-    requires = { 'nvim-tree/nvim-web-devicons' -- optional, for file icons
-    },
-    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+      'nvim-tree/nvim-tree.lua',
+      requires = { 'nvim-tree/nvim-web-devicons' -- optional, for file icons
+      },
+      tag = 'nightly' -- optional, updated every week. (see issue #1193)
   }
 
   --tree sitter
@@ -14,25 +14,25 @@ require('packer').startup(function(use)
 
   -- lsp and cmp
   use {
-    'VonHeikemen/lsp-zero.nvim',
-    requires = {
-      -- LSP Support
-      { 'neovim/nvim-lspconfig' },
-      { 'williamboman/mason.nvim' },
-      { 'williamboman/mason-lspconfig.nvim' },
+      'VonHeikemen/lsp-zero.nvim',
+      requires = {
+          -- LSP Support
+          { 'neovim/nvim-lspconfig' },
+          { 'williamboman/mason.nvim' },
+          { 'williamboman/mason-lspconfig.nvim' },
 
-      -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },
-      { 'hrsh7th/cmp-buffer' },
-      { 'hrsh7th/cmp-path' },
-      { 'saadparwaiz1/cmp_luasnip' },
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'hrsh7th/cmp-nvim-lua' },
+          -- Autocompletion
+          { 'hrsh7th/nvim-cmp' },
+          { 'hrsh7th/cmp-buffer' },
+          { 'hrsh7th/cmp-path' },
+          { 'saadparwaiz1/cmp_luasnip' },
+          { 'hrsh7th/cmp-nvim-lsp' },
+          { 'hrsh7th/cmp-nvim-lua' },
 
-      -- Snippets
-      { 'L3MON4D3/LuaSnip' },
-      { 'rafamadriz/friendly-snippets' },
-    }
+          -- Snippets
+          { 'L3MON4D3/LuaSnip' },
+          { 'rafamadriz/friendly-snippets' },
+      }
   }
 
   -- autopairs
@@ -55,8 +55,8 @@ require('packer').startup(function(use)
 
   -- telescope
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    requires = { { 'nvim-lua/plenary.nvim' } }
+      'nvim-telescope/telescope.nvim', tag = '0.1.0',
+      requires = { { 'nvim-lua/plenary.nvim' } }
   }
 
   -- virtual terminal
@@ -68,13 +68,16 @@ require('packer').startup(function(use)
   use 'sindrets/diffview.nvim'
 
   -- tmux navigation
-  use 'alexghergh/nvim-tmux-navigation'
+  use({
+      "aserowy/tmux.nvim",
+      config = function() return require("tmux").setup() end
+  })
 
   use 'alvan/vim-closetag'
 
   use 'nvim-lualine/lualine.nvim'
 
-  use {'stevearc/dressing.nvim'}
+  use { 'stevearc/dressing.nvim' }
 end)
 
 -- load plugin config
@@ -84,7 +87,6 @@ require("plugins.config.autopairs")
 require('plugins.config.comment')
 require('plugins.config.telescope-config')
 require('plugins.config.git')
-require('plugins.config.navigation')
 require('plugins.config.term')
 require('plugins.config.ts')
 require('plugins.config.line')
