3a24212891af60d59a0d6dc752c18ade3997008c
Damet24 <damet.2401@gmail.com>
2023-03-30 17:16:48 -0500

    [neovim] toggel term commands and telescope keymap added

--- neovim/lua/plugins/config/telescope_config.lua
+++ neovim/lua/plugins/config/telescope_config.lua
@@ -1,5 +1,8 @@
 require('utils')
 require('telescope').setup{ }
 
-map('n', '<leader>f',  "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer =  false }))<cr>", opts)
+local builtin = require('telescope.builtin')
+
+map('n', '<leader>p',  "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer =  false }))<cr>", opts)
+map('n', '<leader>f', builtin.live_grep, opts)
 
