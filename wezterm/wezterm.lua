ab3c3f069325be295b6813e0edbe0a5355562231
Daniel Mercado <damet@pop-os.localdomain>
2023-05-01 21:12:06 -0500

    [wezterm] updated color scheme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,61 +1,10 @@
 local wezterm = require 'wezterm'
 
 return {
-  font = wezterm.font 'DaddyTime Mono NF',
-  font_size = 16,
+  font = wezterm.font 'DaddyTimeMono Nerd Font',
+  font_size = 13,
   line_height = 1.2,
-  -- color_scheme = "Framer (base16)",
-  color_scheme = 'Afterglow',
-
-
-  colors = {
-    -- foreground = '#abb2bf',
-    --     tab_bar = {
-    --         background = 'rgba(30,33,39,95%)',
-    --
-    --         active_tab = {
-    --             bg_color = 'rgba(40,44,52,1)',
-    --             fg_color = '#c0c0c0',
-    --             intensity = 'Normal',
-    --             underline = 'None',
-    --             italic = false,
-    --             strikethrough = false,
-    --         },
-    --
-    --         inactive_tab = {
-    --             bg_color = 'rgba(30,33,39,95%)',
-    --             fg_color = '#808080',
-    --         },
-    --
-    --         inactive_tab_hover = {
-    --             bg_color = 'rgba(30,33,39,95%)',
-    --             fg_color = '#909090',
-    --             italic = true,
-    --
-    --         },
-    --
-    --         new_tab = {
-    --             bg_color = 'rgba(30,33,39,95%)',
-    --             fg_color = '#808080',
-    --
-    --         },
-    --
-    --         new_tab_hover = {
-    --             bg_color = 'rgba(40,44,52,1)',
-    --             fg_color = '#909090',
-    --             italic = true,
-    --
-    --         },
-    --     },
-  },
-
-  -- window_frame = {
-  --     active_titlebar_bg = 'rgba(30,33,39,95%)',
-  --
-  --     inactive_titlebar_bg = 'rgba(30,33,39,95%)',
-  -- },
-
- -- window_background_opacity = 0.85,
+  color_scheme = 'kanagawabones',
   hide_tab_bar_if_only_one_tab = true,
 
   window_padding = {
