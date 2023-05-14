75f118f2284bb19eae1417377265363a90d1d25b
Daniel Mercado <damet.2401@gmail.com>
2023-05-14 13:15:35 -0500

    [wezterm] color teeme defalut

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,8 +3,8 @@ local wezterm = require 'wezterm'
 return {
   font = wezterm.font 'DaddyTimeMono Nerd Font',
   font_size = 13,
-  line_height = 1.2,
-  color_scheme = 'kanagawabones',
+  line_height = 1.4,
+  -- color_scheme = 'kanagawabones',
   hide_tab_bar_if_only_one_tab = true,
 
   window_padding = {
